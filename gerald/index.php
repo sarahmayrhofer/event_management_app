3b31be9c0a20ef8b4dbc7f78e1cf8bc4e6543b42
Gerald <122382776+k12119624@users.noreply.github.com>
2024-04-18 00:47:45 +0200

    name pages more fittingly

--- gerald/index.php
+++ gerald/index.php
@@ -29,7 +29,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Subpage</title>
+    <title>Main page</title>
 </head>
 <body>
     <?php
--- gerald/login.php
+++ gerald/login.php
@@ -18,7 +18,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Keycloak Test</title>
+    <title>Log in</title>
 </head>
 <body>
     <h1>Something went wrong.</h1>
