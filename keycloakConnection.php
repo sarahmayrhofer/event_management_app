85a15377d6e88bc094452cf0a503f5ac70b5096b
Gerald <122382776+k12119624@users.noreply.github.com>
2024-04-26 18:08:31 +0200

    store keycloak info in environment, too

--- keycloakConnection.php
+++ keycloakConnection.php
@@ -1,13 +1,18 @@
 <?php
-    require 'vendor/autoload.php';
+
+    require_once realpath(__DIR__ . "/vendor/autoload.php");
+
+    use Dotenv\Dotenv;
+
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
     use Jumbojett\OpenIDConnectClient;
 
     session_start(); // Also continues existing session
 
-    $keycloakPort = '8081';
-    $idProviderUrl = 'localhost:' . $keycloakPort . '/realms/demo-realm';
-    $clientId = 'demo_client';
-    $clientSecret = 'eSbmLe9GB3K7FUrMmeeHXVOg03W87Yy7';
+    $idProviderUrl = $_ENV['KEYCLOAK_SERVER'] . ':' . $_ENV['KEYCLOAK_PORT'] . $_ENV['KEYCLOAK_REALM_URL'];
+    $clientId = $_ENV['KEYCLOAK_CLIENT_ID'];
+    $clientSecret = $_ENV['KEYCLOAK_CLIENT_SECRET'];
 
     $oidc = new OpenIDConnectClient($idProviderUrl, $clientId, $clientSecret);
