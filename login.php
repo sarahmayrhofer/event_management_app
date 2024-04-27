e41caa4b64d4b1d283cfb4951ee0a2cd1e7e88fe
Gerald <122382776+k12119624@users.noreply.github.com>
2024-04-27 22:04:26 +0200

    tidy up login.php
    Remove unnecessary and unreachable code.

--- login.php
+++ login.php
@@ -2,8 +2,6 @@
     include 'keycloakConnection.php';
 
     if($oidc->authenticate()) {
-        $helloMsg = 'Hello ' . $oidc->requestUserInfo('email') . '!';
-
         // Apparently, this is how you do it, as there doesn't seem to be a way to verify the id token without going via the login.
         // Src: https://stackoverflow.com/a/4860797
         $_SESSION['id'] = $oidc->getIdToken();
@@ -22,16 +20,3 @@
     }
 
     die(); // Stop the PHP processor in case the receiver does not respect the header.
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in</title>
-</head>
-<body>
-    <h1>Something went wrong.</h1>
-</body>
-</html>
