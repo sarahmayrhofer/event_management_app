c97f0e864973801c4dc6d62a3dc46f7f516de24f
Gerald <122382776+k12119624@users.noreply.github.com>
2024-04-29 01:53:01 +0200

    fix typo

--- setup/booking.sql
+++ setup/booking.sql
@@ -5,5 +5,5 @@ CREATE TABLE `booking` (
   `user_name` varchar(255) NOT NULL,
   `no_participants` int(11) NOT NULL DEFAULT 1 CHECK(`no_participants` >= 1),
   `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`user_id`),
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB;
