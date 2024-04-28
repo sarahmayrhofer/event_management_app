6a053066c029455cf54448168442bb2580023dd4
Gerald <122382776+k12119624@users.noreply.github.com>
2024-04-28 04:26:48 +0200

    remove foreign key constraint from booking.user_id

--- setup/booking.sql
+++ setup/booking.sql
@@ -6,6 +6,4 @@ CREATE TABLE `booking` (
   `no_participants` int(11) NOT NULL DEFAULT 1 CHECK(`no_participants` >= 1),
   `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`user_id`),
-  
-  CONSTRAINT `booking_fkey_user_entity` FOREIGN KEY (`user_id`) REFERENCES `user_entity` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
